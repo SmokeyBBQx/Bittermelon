586c926a64481a242433b93313e248888e74485e
Nick10004 <90574812+Nick10004@users.noreply.github.com>
2024-06-11 00:21:42 -0600

    Character command refactor
    - Refactored retrieving character from name
    - Minor variable refactoring

--- src/main/java/net/smokeybbq/bittermelon/commands/character/CommandCharacter.java
+++ src/main/java/net/smokeybbq/bittermelon/commands/character/CommandCharacter.java
@@ -41,14 +41,22 @@ public class CommandCharacter {
         return 1;
     }
 
+    // does this need a try/catch block?
     private static int createCharacter(CommandContext<CommandSourceStack> context) {
         String name = StringArgumentType.getString(context, "name");
         int age = IntegerArgumentType.getInteger(context, "age");
         String description = StringArgumentType.getString(context, "description");
         String emoteColor = StringArgumentType.getString(context, "emoteColor");
+        Character character = getCharacter(context, name);
 
-        Character character = new Character(context.getSource().getPlayer().getUUID(), name, "test", description, "test", age, 1.5, 80, emoteColor);
-        CharacterManager.getInstance().addCharacter(character);
+        if (character != null) {
+            context.getSource().sendFailure(Component.literal("Character '" + character.getName() + "' already exists"));
+            return 0;
+        } else {
+            character = new Character(context.getSource().getPlayer().getUUID(), name, "test", description, "test", age, 1.5, 80, emoteColor);
+            CharacterManager.getInstance().addCharacter(character);
+        }
+
         context.getSource().sendSystemMessage(Component.literal("Character created: " + name));
         return 1;
     }
@@ -56,35 +64,44 @@ public class CommandCharacter {
     private static int switchCharacter(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         String characterName = StringArgumentType.getString(context, "name");
         ServerPlayer player = context.getSource().getPlayerOrException();
+        Character selectedCharacter = getCharacter(context, characterName);
+        Character activeCharacter = CharacterManager.getInstance().getActiveCharacter(player.getUUID());
 
-        List<Character> characters = CharacterManager.getInstance().getCharacters(player.getUUID());
-
-        Optional<Character> selectedCharacter = characters.stream()
-                .filter(c -> c.getName().equalsIgnoreCase(characterName))
-                .findFirst();
-
-        if (!selectedCharacter.isPresent()) {
+        if (selectedCharacter == null) {
             context.getSource().sendFailure(Component.literal("Character not found: " + characterName));
             return 0;
         }
 
-        if (CharacterManager.getInstance().getActiveCharacter(player.getUUID()) != null) {
-            Character currentCharacter = CharacterManager.getInstance().getActiveCharacter(player.getUUID());
+        if (activeCharacter != null) {
             CompoundTag playerData = player.saveWithoutId(new CompoundTag());
-            currentCharacter.savePlayerData(playerData);
+            activeCharacter.savePlayerData(playerData);
         }
 
-        if (selectedCharacter.get().getPlayerData() != null) {
-            player.load(selectedCharacter.get().getPlayerData());
+        if (selectedCharacter.getPlayerData() != null) {
+            player.load(selectedCharacter.getPlayerData());
             player.connection.teleport(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
             player.refreshDimensions();
             player.getInventory().setChanged();
             player.resetSentInfo();
         }
 
-        CharacterManager.getInstance().setActiveCharacter(player, selectedCharacter.get());
-        context.getSource().sendSystemMessage(Component.literal("Character switched: " + selectedCharacter.get().getName()));
+        CharacterManager.getInstance().setActiveCharacter(player, selectedCharacter);
+        context.getSource().sendSystemMessage(Component.literal("Character switched: " + selectedCharacter.getName()));
         return 1;
 
+    }
+
+    // get player characters, NCS
+    private static Character getCharacter(CommandContext<CommandSourceStack> context, String characterName) {
+        List<Character> characters = CharacterManager.getInstance().getCharacters(context.getSource().getPlayer().getUUID());
+
+        Optional<Character> selectedCharacter = characters.stream()
+                .filter(c -> c.getName().equalsIgnoreCase(characterName))
+                .findFirst();
+
+        if (!selectedCharacter.isPresent()) {
+            return null;
+        }
+        return selectedCharacter.get();
     }
 }
