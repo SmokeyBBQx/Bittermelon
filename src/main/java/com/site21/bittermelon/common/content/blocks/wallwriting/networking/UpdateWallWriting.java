542a266fef9f77a092111c6b9c0cb1fe226d1b0b
Nick10004 <90574812+Nick10004@users.noreply.github.com>
2025-12-07 16:33:07 -0600

    Cleanup

--- src/main/java/com/site21/bittermelon/common/content/blocks/wallwriting/networking/UpdateWallWriting.java
+++ src/main/java/com/site21/bittermelon/common/content/blocks/wallwriting/networking/UpdateWallWriting.java
@@ -48,7 +48,7 @@ public record UpdateWallWriting(BlockPos pos, String[] text, boolean isFinal) im
             if (isFinal) {
                 boolean valid = false;
                 for (Component component : wallWriting.getText().getMessages(false)) {
-                    if (!component.toString().equals("empty") & !component.getString().isBlank()) {
+                    if (!component.toString().equals("empty") && !component.getString().isBlank()) {
                         valid = true;
                         break;
                     }
@@ -56,12 +56,12 @@ public record UpdateWallWriting(BlockPos pos, String[] text, boolean isFinal) im
                 if (!valid) {
                     wallWriting.getLevel().setBlock(pos, Blocks.AIR.defaultBlockState(), UPDATE_CLIENTS);
                 } else {
-                    // consume item if out of durability on window close
+                    // Consume item if out of durability on window close
                     ItemStack mainHandItem = ctx.player().getMainHandItem();
                     ItemStack offHandItem = ctx.player().getOffhandItem();
-                    if (mainHandItem.getItem() instanceof WallWriterItem & mainHandItem.getDamageValue() == mainHandItem.getMaxDamage()) {
+                    if (mainHandItem.getItem() instanceof WallWriterItem && mainHandItem.getDamageValue() >= mainHandItem.getMaxDamage()) {
                         mainHandItem.consume(1, ctx.player());
-                    } else if (offHandItem.getItem() instanceof WallWriterItem & offHandItem.getDamageValue() == offHandItem.getMaxDamage()) {
+                    } else if (offHandItem.getItem() instanceof WallWriterItem && offHandItem.getDamageValue() >= offHandItem.getMaxDamage()) {
                         offHandItem.consume(1, ctx.player());
                     }
 
