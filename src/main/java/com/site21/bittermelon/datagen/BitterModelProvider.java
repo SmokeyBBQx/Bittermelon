a05bc79a18794955e547a6ddc41deffece5f6486
PunsterFish <159015328+PunsterFish@users.noreply.github.com>
2026-08-13 14:23:57 -0700

    SCP-025-FR, AND 939 Spawn eggs

--- src/main/java/com/site21/bittermelon/datagen/BitterModelProvider.java
+++ src/main/java/com/site21/bittermelon/datagen/BitterModelProvider.java
@@ -161,6 +161,7 @@ public class BitterModelProvider extends ModelProvider {
         itemModels.generateFlatItem(BitterItems.SCP_939_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(BitterItems.SCP_1507_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(SCP_718_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM);
+        itemModels.generateFlatItem(SCP_025_FR_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM);
 
         // Tools and Equipment
         itemModels.generateFlatItem(LASER_DESIGNATOR.get(), ModelTemplates.FLAT_HANDHELD_ITEM);
