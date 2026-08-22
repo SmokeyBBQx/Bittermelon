ad7f83b7d472677917ebcf771bc58c6466c1ded8
SmokeyBBQx <47431313+SmokeyBBQx@users.noreply.github.com>
2026-08-23 01:56:07 +0200

    amnesia inventory shuffling

--- src/main/java/com/site21/bittermelon/common/content/mobeffects/amnesia/AmnesiaEffect.java
+++ src/main/java/com/site21/bittermelon/common/content/mobeffects/amnesia/AmnesiaEffect.java
@@ -1,8 +1,17 @@
 package com.site21.bittermelon.common.content.mobeffects.amnesia;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AmnesiaEffect extends MobEffect {
     public AmnesiaEffect() {
@@ -11,6 +20,34 @@ public class AmnesiaEffect extends MobEffect {
 
     @Override
     public void onEffectStarted(LivingEntity mob, int amplifier) {
+        if (mob instanceof ServerPlayer player) {
+            shuffleInventory(player);
+        }
+    }
 
+    public static void shuffleInventory(ServerPlayer player) {
+        Inventory inventory = player.getInventory();
+        IntList slots = new IntArrayList();
+        List<ItemStack> stacks = new ArrayList<>();
+        for (int i = 0; i < Inventory.INVENTORY_SIZE; i++) {
+            ItemStack stack = inventory.getItem(i);
+            if (!stack.isEmpty()) {
+                slots.add(i);
+                stacks.add(stack);
+            }
+        }
+
+        if (stacks.size() < 2) {
+            return;
+        }
+
+        for (int i = stacks.size() - 1; i > 0; i--) {
+            Collections.swap(stacks, i, player.level().getRandom().nextInt(i + 1));
+        }
+
+        for (int i = 0; i < slots.size(); i++) {
+            int slot = slots.getInt(i);
+            inventory.setItem(slot, stacks.get(i));
+        }
     }
 }
