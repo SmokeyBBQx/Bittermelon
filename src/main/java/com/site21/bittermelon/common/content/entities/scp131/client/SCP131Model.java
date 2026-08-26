43ebb516e2d0b62072b490f5941c257a304a4126
PunsterFish <159015328+PunsterFish@users.noreply.github.com>
2026-08-25 19:19:53 -0700

    SCP-131 walking animation

--- src/main/java/com/site21/bittermelon/common/content/entities/scp131/client/SCP131Model.java
+++ src/main/java/com/site21/bittermelon/common/content/entities/scp131/client/SCP131Model.java
@@ -1,18 +1,25 @@
 package com.site21.bittermelon.common.content.entities.scp131.client;
 
+import com.site21.bittermelon.client.animation.SCP131Walk;
+import net.minecraft.client.animation.KeyframeAnimation;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
 import org.jetbrains.annotations.NotNull;
-
 
 public class SCP131Model extends EntityModel<SCP131RenderState> {
     private final ModelPart body;
+    private final KeyframeAnimation walk;
 
     protected SCP131Model(ModelPart root) {
         super(root);
         body = root.getChild("body");
+        walk = SCP131Walk.WALK.bake(root);
     }
 
     public static @NotNull LayerDefinition createBodyLayer() {
@@ -43,7 +50,9 @@ public class SCP131Model extends EntityModel<SCP131RenderState> {
     public void setupAnim(@NotNull SCP131RenderState renderState) {
         super.setupAnim(renderState);
 
-        body.xRot = renderState.xRot * ((float)Math.PI / 180F);
-        body.yRot = renderState.yRot * ((float)Math.PI / 180F);
+        body.xRot = renderState.xRot * ((float) Math.PI / 180F);
+        body.yRot = renderState.yRot * ((float) Math.PI / 180F);
+
+        this.walk.apply(renderState.walkAnimationState, renderState.ageInTicks);
     }
 }
--- src/main/java/com/site21/bittermelon/common/content/entities/scp131/client/SCP131RenderState.java
+++ src/main/java/com/site21/bittermelon/common/content/entities/scp131/client/SCP131RenderState.java
@@ -1,8 +1,9 @@
 package com.site21.bittermelon.common.content.entities.scp131.client;
 
+import net.minecraft.world.entity.AnimationState;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
-
 
 public class SCP131RenderState extends LivingEntityRenderState {
     public int variant;
+    public AnimationState walkAnimationState = new AnimationState();
 }
